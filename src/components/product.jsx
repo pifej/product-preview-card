bccb65c3ec3d788f82d6f741fc0f1b964649e21b
Peter Meszaros <peter.m93@gmail.com>
2026-03-21 19:11:40 +0100

    one commit to rule them all

---
+++ src/components/product.jsx
@@ -0,0 +1,34 @@
+import "./product.css";
+import prodPic from "../assets/images/image-product-desktop.jpg";
+import cartPic from "../assets/images/icon-cart.svg";
+
+export default function Product(){
+    let productImage = new Image();
+    productImage.src = prodPic;
+    let productType = "PERFUME";
+    let productName = "Gabrielle Essence Eau De Parfum";
+    let productDescription = "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.";
+    let productPrice = "$149.99";
+    let productOriginalPrice = "$169.99";
+    return(
+        <div className="product-main">
+            <div className="product-image">
+                <img src={prodPic} alt="product image" />
+            </div>
+            <div className="product-info">
+                <p className="product-type-txt">{productType}</p>
+                <p className="product-name-txt">{productName}</p>
+                <p className="product-detail-txt">{productDescription}</p>
+                <p className="product-price-main">
+                    <p className="product-price-off-txt">{productPrice}</p>
+                    <p className="product-price-txt">{productOriginalPrice}</p>
+                </p>
+                <button className="buy-button">
+                    {/* <span className="button-add-to-cart-icon">{productImage}</span> */}
+                    <img className="button-add-to-cart-icon" src={cartPic} alt="cart icon" />
+                    <span className="button-add-to-cart-txt">Add to Cart</span>
+                </button>
+            </div>
+        </div>
+    )
+}
