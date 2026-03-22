8f56336f8ce1047b1f0a25c5126a1e23d6ec13e7
Peter Meszaros <peter.m93@gmail.com>
2026-03-22 17:22:36 +0100

    added media query for mobile view

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,6 +1,7 @@
 import "./product.css";
 import prodPic from "../assets/images/image-product-desktop.jpg";
 import cartPic from "../assets/images/icon-cart.svg";
+import { useState } from "react";
 
 export default function Product(){
     let productImage = new Image();
@@ -10,6 +11,7 @@ export default function Product(){
     let productDescription = "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.";
     let productPrice = "$149.99";
     let productOriginalPrice = "$169.99";
+
     return(
         <div className="product-main">
             <div className="product-image">
@@ -19,10 +21,10 @@ export default function Product(){
                 <p className="product-type-txt">{productType}</p>
                 <p className="product-name-txt">{productName}</p>
                 <p className="product-detail-txt">{productDescription}</p>
-                <p className="product-price-main">
+                <div className="product-price-main">
                     <p className="product-price-off-txt">{productPrice}</p>
                     <p className="product-price-txt">{productOriginalPrice}</p>
-                </p>
+                </div>
                 <button className="buy-button">
                     {/* <span className="button-add-to-cart-icon">{productImage}</span> */}
                     <img className="button-add-to-cart-icon" src={cartPic} alt="cart icon" />
